5d64aa1f7c287dd6149766b1a07bec595f51ec98
Ruben Barylak <rbar34@student.monash.edu>
2023-10-10 17:49:22 +1100

    test both predictions and random test output using aequitas

--- test_aq.py
+++ test_aq.py
@@ -4,42 +4,41 @@ from aequitas.preprocessing import preprocess_input_df
 from glob import glob
 from pathlib import Path
 
-# IMPORTANT: Run harness.py first to generate csv test results
+# IMPORTANT: Run harness.py and random_test_harness.py first to generate csv test results
 
-# TODO: write some loop to handle both paths
 PATHS = {
-        
-        }
+    "aequitas": r'./predictions/*.csv',
+    "random_generated_group_fairness": r'./generated_tests/*.csv'
+}
 
-# DIR_PATH = r'./predictions/*.csv'
-DIR_PATH = r'./generated_tests/*.csv'
-prediction_files = glob(DIR_PATH)
 
-for prediction_file in prediction_files:
-    print(prediction_file)
+for destination, predictions in PATHS.items():
 
-    df = pd.read_csv(prediction_file)
+    prediction_files = glob(predictions)
 
-    # rename columns to work properly with AEQUITAS
-    df.rename(columns={'Predicted_Labels': 'score',
-              'True_Labels': 'label_value'}, inplace=True)
+    for prediction_file in prediction_files:
+        print(prediction_file)
 
-    # preprocess for AEQUITAS
-    df, _ = preprocess_input_df(df)
+        df = pd.read_csv(prediction_file)
 
-    # compute group metrics
-    g = Group()
-    xtab, _ = g.get_crosstabs(df)
-    absolute_metrics = g.list_absolute_metrics(xtab)
+        # rename columns to work properly with AEQUITAS
+        df.rename(columns={'Predicted_Labels': 'score',
+                  'True_Labels': 'label_value'}, inplace=True)
 
-    absolute_metrics_per_population_group = xtab[[
-        'attribute_name', 'attribute_value'] + absolute_metrics].round(2)
+        # preprocess for AEQUITAS
+        df, _ = preprocess_input_df(df)
 
-    # TODO: integrate this into the harness
-    pathname = f"./random_generated_group_fairness/{prediction_file.split('/')[-1]}"
-    # pathname = f"./aequitas/{prediction_file.split('/')[-1]}"
-    filepath = Path(pathname)
-    filepath.parent.mkdir(parents=True, exist_ok=True)
+        # compute group metrics
+        g = Group()
+        xtab, _ = g.get_crosstabs(df)
+        absolute_metrics = g.list_absolute_metrics(xtab)
 
-    absolute_metrics_per_population_group.to_csv(filepath, index=False)
-    print(absolute_metrics_per_population_group)
+        absolute_metrics_per_population_group = xtab[[
+            'attribute_name', 'attribute_value'] + absolute_metrics].round(2)
+
+        # TODO: integrate this into the harness
+        pathname = f"./{destination}/{prediction_file.split('/')[-1]}"
+        filepath = Path(pathname)
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+
+        absolute_metrics_per_population_group.to_csv(filepath, index=False)
