d58067e2eac9b9cbb02205611869e3729c87b6d2
Ruben Barylak <rbar34@student.monash.edu>
2023-10-15 23:18:16 +1100

    begin hypothesis testing

---
+++ hypothesis_testing.py
@@ -0,0 +1,14 @@
+from scipy.stats import spearmanr, ttest_rel
+
+# LOADING DATA
+# import the aequitas group metrics from a) the naive random, b) the directed approach
+# calculate the individual test metrics from a) the naive random, b) the directed approach
+
+# CALCULATING
+# group metrics: calculate the max-min metrics from each file in a) the naive random, b) the directed approach
+
+# RQ1
+# perform repeated measure t-tests grouped by each metric, comparing the means of a) the naive random, b) the directed approach
+
+# RQ2
+# perform spearman rho of each metric against each other metric
