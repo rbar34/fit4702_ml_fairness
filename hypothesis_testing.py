44787538cd0d873728392d8c50fbbf4d9368dec9
Ruben Barylak <rbar34@student.monash.edu>
2023-10-18 11:56:45 +1100

    aggregate data for RQ1

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -1,40 +1,49 @@
 from scipy.stats import spearmanr, ttest_rel
 import pandas as pd
 from glob import glob
+from pathlib import Path
 
 DIRECTORIES = {
-    "random_individual": r'./generated_tests',
-    "random_group": r'./random_generated_group_fairness/',
-    "directed_individual": r'./total_tests/',
-    "directed_group": r'./aequitas/',
+    "individual": {
+        "random_individual": r'./failed_tests_random/',
+        "directed_individual": r'./failed_tests_directed/', },
+    "group": {
+        "random_group": r'./group_metrics_random/',
+        "directed_group": r'./group_metrics_directed/', },
 }
 
-random_group_files = glob(DIRECTORIES['random_group'])
-for random_group_file in random_group_files:
-    if 'german' in random_group_file:
-        sensitive_attribute = 'personal_status_sex_'
-        separator = 'A'
-        default_category = {sensitive_attribute: '99'}
-    elif 'adult' in random_group_file:
-        sensitive_attribute = 'sex'
-        separator = '_'
-        default_category = {sensitive_attribute: 'Female'}
-    elif 'compas' in random_group_file:
-        sensitive_attribute = 'sex'
-        separator = '_'
-        default_category = {sensitive_attribute: 'Female'}
+GROUP_METRICS = [
+    'tpr', 'fpr', 'tnr', 'fnr', 'precision',
+]
 
-# separate by datasets
+SENSITIVE_ATTRIBUTES = {
+    'german': 'personal_status_sex_',
+    'adult': 'sex',
+    'compas': 'sex',
+}
 
+file_base_names = []
+for e in Path(DIRECTORIES['group']['random_group']).iterdir():
+    file_base_names.append(e.name)
 
-# LOADING DATA
-# import the aequitas group metrics from a) the naive random, b) the directed approach
-
+# RQ1: KS Testing
+for dataset, sensitive_attribute in SENSITIVE_ATTRIBUTES.items():
+    for test_approach, directory in DIRECTORIES['group'].items():
+        print(test_approach, dataset)
+        # get the files
+        group_files = filter(lambda filename: dataset in filename, file_base_names)
+        min_max_differences = []
+        for group_file in group_files:
+            dataframe = pd.read_csv(Path(f"{directory}{group_file}"))
+            # select the sensitive attributes
+            raw_data = dataframe.loc[dataframe['attribute_name'] == sensitive_attribute, GROUP_METRICS]
+            min_max_differences.append(raw_data.max() - raw_data.min())
+        aggregated_metrics = pd.DataFrame(min_max_differences)
+        # KS Test
+        # for each metric: random vs directed
+        print(aggregated_metrics)
 
 # calculate the individual test metrics from a) the naive random, b) the directed approach
-
-# CALCULATING
-# group metrics: calculate the max-min metrics from each file in a) the naive random, b) the directed approach
 
 # RQ1
 # perform repeated measure t-tests grouped by each metric, comparing the means of a) the naive random, b) the directed approach
