64181a65eeb8bfd9d268f32dbc3a30ab32d90187
Ruben Barylak <rbar34@student.monash.edu>
2023-10-18 14:56:14 +1100

    add statistical tests

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -1,4 +1,4 @@
-from scipy.stats import spearmanr, ttest_rel
+from scipy.stats import spearmanr, wilcoxon
 import pandas as pd
 from glob import glob
 from pathlib import Path
@@ -27,9 +27,12 @@ for e in Path(DIRECTORIES['group']['random_group']).iterdir():
     file_base_names.append(e.name)
 
 # RQ1: KS testing
+print('\nRQ1')
+print('-'*10)
 for dataset, sensitive_attribute in SENSITIVE_ATTRIBUTES.items():
+    results = []
     for test_approach, directory in DIRECTORIES['group'].items():
-        # print(test_approach, dataset)
+        print('\n', test_approach, dataset)
         # get the files
         group_files = filter(lambda filename: dataset in filename, file_base_names)
         min_max_differences = []
@@ -39,15 +42,21 @@ for dataset, sensitive_attribute in SENSITIVE_ATTRIBUTES.items():
             raw_data = dataframe.loc[dataframe['attribute_name'] == sensitive_attribute, GROUP_METRICS]
             min_max_differences.append(raw_data.max() - raw_data.min())
         aggregated_metrics = pd.DataFrame(min_max_differences)
+        results.append(aggregated_metrics)
         # KS test
         # for each metric: random vs directed
         # print(aggregated_metrics)
+    for group_metric in GROUP_METRICS:
+        print(wilcoxon(results[0][group_metric], results[1][group_metric]))
 
+
+print('\nRQ2')
+print('-'*10)
 # RQ2: Spearman rho testing
 for dataset, sensitive_attribute in SENSITIVE_ATTRIBUTES.items():
     group_files = filter(lambda filename: dataset in filename, file_base_names)
     min_max_differences = []
-    print(dataset)
+    print('\n', dataset)
     for group_file in group_files:
         for test_approach, directory in DIRECTORIES['group'].items():
             dataframe = pd.read_csv(Path(f"{directory}{group_file}"))
@@ -57,7 +66,11 @@ for dataset, sensitive_attribute in SENSITIVE_ATTRIBUTES.items():
     aggregated_metrics = pd.DataFrame(min_max_differences)
     # Spearman rho test
     # for each metric: random vs directed
-    print(aggregated_metrics)
+    # print(aggregated_metrics)
+    for metric_a in GROUP_METRICS:
+        for metric_b in GROUP_METRICS:
+            if metric_a != metric_b:
+                print(spearmanr(aggregated_metrics[metric_a], aggregated_metrics[metric_b]))
 
 # calculate the individual test metrics from a) the naive random, b) the directed approach
 
