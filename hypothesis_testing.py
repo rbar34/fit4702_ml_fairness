0c951534bf94101922fea7dc0c8e3d71a679c2ef
Ruben Barylak <rbar34@student.monash.edu>
2023-10-18 22:55:57 +1100

    add pair-plots for RQ2

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -1,6 +1,10 @@
 from scipy.stats import spearmanr, wilcoxon
 import pandas as pd
 from pathlib import Path
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
 
 TESTS_RUN = 1000
 
@@ -73,7 +77,6 @@ for dataset, sensitive_attribute in SENSITIVE_ATTRIBUTES.items():
 
     # write to file
     wilcoxon_df.to_csv(filepath, index=True)
-    print(wilcoxon_df)
 
 
 print('\nRQ2')
@@ -113,15 +116,18 @@ for dataset, sensitive_attribute in SENSITIVE_ATTRIBUTES.items():
                 correlations_df.loc[metric_a, metric_b] = score.statistic
                 p_values_df.loc[metric_a, metric_b] = score.pvalue
 
-    # ensure directory exists
+    # create pair-plot
+    pairplot = sns.pairplot(data=aggregated_metrics)
+    figure_filepath = Path(f'./statistical_results/rq2_{dataset}_pairplot.png')
+    figure_filepath.parent.mkdir(parents=True, exist_ok=True)
+    plt.savefig(fname=figure_filepath, format='png')
+
+    # save correlations and p-values
     correltions_filepath = Path(
         f'./statistical_results/rq2_{dataset}_correlation.csv')
     pvalues_filepath = Path(f'./statistical_results/rq2_{dataset}_pvalues.csv')
     correltions_filepath.parent.mkdir(parents=True, exist_ok=True)
     pvalues_filepath.parent.mkdir(parents=True, exist_ok=True)
 
-    # write to file
     correlations_df.to_csv(correltions_filepath, index=True, na_rep='NA')
     p_values_df.to_csv(pvalues_filepath, index=True, na_rep='NA')
-
-    print(correlations_df)
